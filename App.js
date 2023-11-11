26ac865aadff0044470c55b9a3944410706fd0a2
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2023-11-11 14:53:01 +0700

    update

--- App.js
+++ App.js
@@ -1,27 +1,90 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('')
   return (
     <View style={styles.container}>
-      <Button style={styles.btn}Submit</Button>
-      <Text style={styles.text}>Hello World</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.textHeader}>goKantin</Text>
+    <TextInput style={styles.input}placeholder="Search what u need.."
+    onChangeText={(text) => {
+          setText(text) }} />
+    <Text style={styles.text}>
+      {'\n'}Your need: {text}</Text>
+
+    <ScrollView style={styles.containerView}>
+      <View style={styles.large} />
+      <ScrollView horizontal>
+        <View style={styles.small}>
+          <Text style={{color: 'white', textAlign:'center', marginTop: 150,}}>Starbucks</Text>
+        </View>
+        <View style={styles.small}>
+          <Text style={{color: 'white', textAlign:'center', marginTop: 150,}}>Boba</Text>
+        </View>
+        <View style={styles.small}>
+          <Text style={{color: 'white', textAlign:'center', marginTop: 150,}}>Ice Cream</Text>
+        </View>
+        <View style={styles.small}>
+          <Text style={{color: 'white', textAlign:'center', marginTop: 150,}}>Coffe</Text>
+        </View>
+
+      </ScrollView>
+      <View style={styles.large} />
+      <ScrollView horizontal>
+        <View style={styles.small} />
+        <View style={styles.small} />
+      </ScrollView>
+      <View style={styles.large} />
+    </ScrollView>
+    <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#362789',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    fontSize: 40,
-    fontStyle: 'italic',
-    color: 'white',
+    container: {
+      flex: 1,
+      backgroundColor: '#a6dbbc',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    containerView: {
+      flex: 1,
+    },
+    small: {
+      width: 150,
+      height: 200,
+      marginBottom: 10,
+      marginRight: 10,
+      backgroundColor: '#5fb2ff',
+    },
+    large: {
+      width: 400,
+      height: 300,
+      marginBottom: 10,
+      marginRight: 10,
+      backgroundColor: 'skyblue',
+    },
+    textHeader: {
+      fontSize: 40,
+      marginTop: 30,
+      color: 'white',
+    },
+    input:{
+      backgroundColor: 'white',
+      borderRadius: 10,
+      marginTop: 30,
+      width: 200,
+      padding: 5,
+      fontStyle: 'italic',
+    },
+    text:{
+      fontSize: 16,
+      color: 'white',
+      textAlign: 'left',
+    
   }
-});
+})
+
+  
